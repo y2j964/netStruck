ce3c6d6a06db7abcaa7488073eef3124b1cd6c8b
Justin Mooney <y2j964@aol.com>
2019-09-29 20:42:15 -0400

    remove endCloneMountAnimation

--- src/components/Tiles/InfiniteTileGroup.js
+++ src/components/Tiles/InfiniteTileGroup.js
@@ -54,11 +54,6 @@ const createEndClones = (slidesPerPosition, filmGroupData) => {
   return endClones;
 };
 
-const endCloneMountAnimation = {
-  animationDuration: '500ms',
-  animationName: 'slideInFromLeft',
-};
-
 function InfiniteTileGroup({
   filmGroupData,
   xPosition,
@@ -110,7 +105,6 @@ function InfiniteTileGroup({
       {...film}
       key={film.id}
       index={index}
-      style={slidesPerPosition === index + 1 ? endCloneMountAnimation : {}}
       placementInViewport={
         index + 1 !== slidesPerPosition ? 'offscreen' : 'leftPreview'
       }
@@ -171,187 +165,7 @@ InfiniteTileGroup.propTypes = {
 
 export default InfiniteTileGroup;
 
-// import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import uuid from 'uuid';
-// import { CSSTransition, TransitionGroup } from 'react-transition-group';
-// import Tile from './Tile';
-
-// const getTilePlaceholders = slidesPerPosition => {
-//   const tileList = [];
-//   for (let i = 0; i < slidesPerPosition; i += 1) {
-//     tileList.push(<Tile key={i} />);
-//   }
-//   return tileList;
+// const endCloneMountAnimation = {
+//   animationDuration: '500ms',
+//   animationName: 'slideInFromLeft',
 // };
-
-// const getPlacementInViewPort = (visibleSlideIndexes, index) => {
-//   if (index === visibleSlideIndexes[0] - 1) {
-//     return 'leftPreview';
-//   }
-//   if (index === visibleSlideIndexes[visibleSlideIndexes.length - 1] + 1) {
-//     return 'rightPreview';
-//   }
-//   if (!visibleSlideIndexes.includes(index)) {
-//     return 'offscreen';
-//   }
-//   if (index === visibleSlideIndexes[0]) {
-//     return 'leftEdge';
-//   }
-//   if (index === visibleSlideIndexes[visibleSlideIndexes.length - 1]) {
-//     return 'rightEdge';
-//   }
-//   return 'middle';
-// };
-
-// const createFrontClones = (slidesPerPosition, filmGroupData) => {
-//   const frontClones = [];
-//   for (let i = 0; i < slidesPerPosition; i += 1) {
-//     const tile = filmGroupData[i];
-//     tile.id = uuid.v4();
-//     frontClones.push(tile);
-//   }
-//   return frontClones;
-// };
-
-// const createEndClones = (slidesPerPosition, filmGroupData) => {
-//   const endClones = [];
-//   for (
-//     let i = filmGroupData.length - slidesPerPosition;
-//     i < filmGroupData.length;
-//     i += 1
-//   ) {
-//     const tile = filmGroupData[i];
-//     tile.id = uuid.v4();
-//     endClones.push(tile);
-//   }
-//   return endClones;
-// };
-
-// function InfiniteTileGroup({
-//   filmGroupData,
-//   xPosition,
-//   transition,
-//   wrapAround,
-//   slidesPerPosition,
-//   visibleSlideIndexes,
-// }) {
-//   const [endClones, setEndClones] = useState([]);
-//   const [frontClones, setFrontClones] = useState([]);
-//   const [leftEdgeIsHovered, setLeftEdgeIsHovered] = useState(false);
-//   const [rightEdgeIsHovered, setRightEdgeIsHovered] = useState(false);
-//   const [middleHoveredIndex, setMiddleHoveredIndex] = useState(null);
-
-//   useEffect(() => {
-//     const endClonesFrags = createEndClones(slidesPerPosition, filmGroupData);
-
-//     const frontClonesFrags = createFrontClones(
-//       slidesPerPosition,
-//       filmGroupData,
-//     );
-
-//     setEndClones(endClonesFrags);
-//     setFrontClones(frontClonesFrags);
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [slidesPerPosition, visibleSlideIndexes]);
-//   // omitting filmGroupData b/c I don't care if clones have updated info;
-//   // they are window dressing that the user never interacts with
-
-//   const tileGroupStyle = {
-//     transform: `translateX(${xPosition}%)`,
-//     transition: transition ? 'transform 400ms ease-in-out' : 'none',
-//   };
-
-//   const clonedFrontTileFrags = frontClones.map((film, index) => (
-//     <Tile
-//       {...film}
-//       key={film.id}
-//       index={index + filmGroupData.length + slidesPerPosition}
-//       placementInViewport={getPlacementInViewPort(
-//         visibleSlideIndexes,
-//         index + filmGroupData.length + slidesPerPosition,
-//       )}
-//       leftEdgeIsHovered={leftEdgeIsHovered}
-//       rightEdgeIsHovered={rightEdgeIsHovered}
-//       middleHoveredIndex={middleHoveredIndex}
-//     />
-//   ));
-//   const clonedEndTileFrags = endClones.map((film, index) => {
-//     return (
-//       // <CSSTransition
-//       //   key={film.id}
-//       //   in={slidesPerPosition !== null}
-//       //   timeout={500}
-//       //   unmountOnExit
-//       //   classNames='end-clones'
-//       // >
-//       <Tile
-//         {...film}
-//         key={film.id}
-//         index={index}
-//         placementInViewport={getPlacementInViewPort(visibleSlideIndexes, index)}
-//         leftEdgeIsHovered={leftEdgeIsHovered}
-//         rightEdgeIsHovered={rightEdgeIsHovered}
-//         middleHoveredIndex={middleHoveredIndex}
-//       />
-//       // </CSSTransition>
-//     );
-//   });
-
-//   const naturalTileFrags = filmGroupData.map((film, index) => (
-//     <Tile
-//       {...film}
-//       key={film.id}
-//       placementInViewport={getPlacementInViewPort(
-//         visibleSlideIndexes,
-//         index + slidesPerPosition,
-//       )}
-//       rowLength={filmGroupData.length}
-//       ariaLabel={`slide ${index + 1} of ${filmGroupData.length}`}
-//       index={index + slidesPerPosition}
-//       // index value is accumulated to account for slides
-//       leftEdgeIsHovered={leftEdgeIsHovered}
-//       handleLeftEdgeIsHovered={setLeftEdgeIsHovered}
-//       rightEdgeIsHovered={rightEdgeIsHovered}
-//       handleRightEdgeIsHovered={setRightEdgeIsHovered}
-//       middleHoveredIndex={middleHoveredIndex}
-//       handleMiddleHoveredIndex={setMiddleHoveredIndex}
-//     />
-//   ));
-
-//   return (
-//     <ul
-//       className='tile-group'
-//       style={tileGroupStyle}
-//       onTransitionEnd={wrapAround}
-//     >
-//       {clonedEndTileFrags.length !== slidesPerPosition && (
-//         <React.Fragment>
-//           {getTilePlaceholders(slidesPerPosition)}
-//         </React.Fragment>
-//       )}
-//       {/* <CSSTransition
-//         in={modalIsOpen}
-//         timeout={300}
-//         unmountOnExit
-//         classNames='modal'
-//         > */}
-//       {/* <TransitionGroup component={null}> */}
-//       {clonedEndTileFrags}
-//       {/* </TransitionGroup> */}
-//       {naturalTileFrags}
-//       {clonedFrontTileFrags}
-//     </ul>
-//   );
-// }
-
-// InfiniteTileGroup.propTypes = {
-//   filmGroupData: PropTypes.array,
-//   wrapAround: PropTypes.func.isRequired,
-//   slidesPerPosition: PropTypes.number.isRequired,
-//   visibleSlideIndexes: PropTypes.array.isRequired,
-//   xPosition: PropTypes.number.isRequired,
-//   transition: PropTypes.bool.isRequired,
-// };
-
-// export default InfiniteTileGroup;
